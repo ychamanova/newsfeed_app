cc402c88090eb658514f83a0950a4054d3c12577
Yelena Chamanova <ychamanova@gmail.com>
2021-01-08 18:30:41 -0800

    update header component

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -1,5 +1,30 @@
+import React, { useState } from 'react';
 import './styles.css';
 
 export const Header = () => {
-  return <header className='Header'> NewsFeed</header>;
+  const [searchTerm, setSearchTerm] = useState('');
+  return (
+    <header>
+      <div className='header-top'>
+        <div className='header-logo'>NewsFeed</div>
+        <form className='header-search'>
+          <label>
+            Search Articles
+            <input
+              className='header-search-input'
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+              defaultValue='e.x. recipes'
+              type='text'
+            />
+          </label>
+        </form>
+      </div>
+      <div className='header-slogan'>
+        <div className='header-slogan-large'>The Best News</div>
+        <br />
+        <div className='header-slogan-small'>From Around the World</div>
+      </div>
+    </header>
+  );
 };
