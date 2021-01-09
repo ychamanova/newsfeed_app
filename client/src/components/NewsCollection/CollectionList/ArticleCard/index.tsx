848b66a8f1be2b35a4cd6755be08447173dc9c57
Yelena Chamanova <ychamanova@gmail.com>
2021-01-09 15:56:21 -0800

    add expandable link to article card

--- client/src/components/NewsCollection/CollectionList/ArticleCard/index.tsx
+++ client/src/components/NewsCollection/CollectionList/ArticleCard/index.tsx
@@ -1,6 +1,12 @@
 import './styles.css';
 
 import Card from '@material-ui/core/Card';
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { OpenInNew } from '@material-ui/icons';
 
 interface Props {
   title: string;
@@ -10,12 +16,29 @@ interface Props {
 
 export const ArticleCard: React.FC<Props> = ({ title, url, abstract }) => {
   return (
-    <button>
-      <Card className='Card'>
-        <h2>{title}</h2>
-        <div>{abstract}</div>
-        <div> Link: {url}</div>
-      </Card>
-    </button>
+    <Card className='Card card-abstract'>
+      <h1>{title}</h1>
+      <br />
+      {abstract}
+      <br />
+      <Accordion className='card-readmore'>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls='panel1a-content'
+          id='panel1a-header'
+        >
+          <Typography>Read More</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            <a href={url} target='_blank' rel='noreferrer'>
+              Source: New York Times. <OpenInNew />
+            </a>
+          </Typography>
+        </AccordionDetails>
+        <div></div>
+        <div></div>
+      </Accordion>
+    </Card>
   );
 };
