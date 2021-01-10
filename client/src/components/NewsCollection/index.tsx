80932219076cb090acba3deac04bed5e88bf2466
Yelena Chamanova <ychamanova@gmail.com>
2021-01-10 14:48:40 -0800

    extract fetch search functionality

--- client/src/components/NewsCollection/index.tsx
+++ client/src/components/NewsCollection/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Button from '@material-ui/core/Button';
 import ButtonGroup from '@material-ui/core/ButtonGroup';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import './styles.css';
 import { categories } from './categories';
 import { ArticleList } from './ArticleList';
@@ -9,25 +10,32 @@ import { Article, SearchItem } from '../../types';
 
 export const NewsCollection = () => {
   const [searchTerm, setSearchTerm] = useState('');
-
   const [category, setCategory] = useState(categories.popularToday);
-
   const [newsItems, setNewsItems] = useState<Article[]>([]);
-
   const [searchItems, setSearchItems] = useState<SearchItem[]>([]);
+  const [loading, setLoading] = useState(true);
 
-  const performSearch = (term: string) => {
-    fetch(`/search?searchTerm=${searchTerm}`).then((data) => {
-      data.json().then((converted) => {
-        setSearchItems(converted.docs);
-      });
-    });
+  const performSearch = async (term: string) => {
+    setCategory(categories.search);
+    setLoading(true);
+    const results = await fetchSearchResults(term);
+    setSearchItems(results);
+    setLoading(false);
+  };
+
+  const fetchSearchResults = async (searchTerm: string) => {
+    const data = await fetch(`/search?searchTerm=${searchTerm}`);
+    const converted = await data.json();
+    return converted.docs;
   };
 
   const getNews = (category: string) => {
+    setSearchTerm('');
+    setLoading(true);
     fetch(`/news/${category}`).then((data) =>
       data.json().then((converted) => {
         setNewsItems(converted);
+        setLoading(false);
       })
     );
   };
@@ -55,9 +63,6 @@ export const NewsCollection = () => {
         <Button onClick={(e) => setCategory(categories.popularThisWeek)}>
           Popular in the Last Week
         </Button>
-        <Button>Arts</Button>
-        <Button>Technology</Button>
-        <Button>Design</Button>
       </ButtonGroup>
       <form>
         <label>
@@ -73,12 +78,16 @@ export const NewsCollection = () => {
           value='Submit'
           onClick={(e) => {
             e.preventDefault();
-            setCategory(categories.search);
+            performSearch(searchTerm);
           }}
         />
       </form>
       <div className='news-collection-container'>
-        {category !== 'search' ? (
+        {loading ? (
+          <div className='news-collection-loader'>
+            <CircularProgress color='inherit' />
+          </div>
+        ) : category !== 'search' ? (
           <ArticleList articlesArray={newsItems} />
         ) : (
           <SearchList searchItems={searchItems} />
